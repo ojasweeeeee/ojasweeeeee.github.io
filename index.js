1b3350b51d52258270b20a704de6657c810a1efa
Ojaswi Saxena <ojaswisaxena14@gmail.com>
2022-12-29 23:13:18 +0530

    Added dark mode option

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const css=document.querySelector("#darkmode-toggle").addEventListener();
+function changeCSS(){
+    var checkbox = document.getElementById('darkmode-toggle');
+    if (checkbox.checked != true)
+    {
+        document.getElementById("theme").setAttribute("href", "css/styles.css");
+    }
+    else
+    {
+        document.getElementById("theme").setAttribute("href", "css/stylesdark.css");
+    }
+}
+
+
 
